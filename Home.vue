e9577940380d4236816151b952f44252a35ad3d7
LangLangyao <18235923269@163.com>
2019-06-26 19:56:37 +0800

    vue练习

---
+++ Home.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <h3>我是Home组件</h3>
+    <Index></Index>   
+</div>
+</template>
+<script>
+import Index from "./index"
+export default {
+    data : function(){
+        return{
+            mages:"马啸天。。。脑补"
+        }
+    },
+    components:{
+        Index
+    }
+}
+</script>
---
+++ index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>{{mag}}</h1>
+    </div>
+</template>
+<script>
+export default {
+     data:function(){
+        return{
+            mag:"姚燕青是大。。。脑补"
+        }
+    } ,
+}
+</script>
